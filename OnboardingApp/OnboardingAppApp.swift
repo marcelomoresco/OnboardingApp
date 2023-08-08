dc4c083a3e4d8d03597bfcabff0960c307e7c6c9
marcelomoresco <marcelomoresco0@gmail.com>
2023-08-08 19:41:03 -0300

    Initial Commit

---
+++ OnboardingApp/OnboardingAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  OnboardingAppApp.swift
+//  OnboardingApp
+//
+//  Created by Marcelo Moresco on 08/08/23.
+//
+
+import SwiftUI
+
+@main
+struct OnboardingAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
